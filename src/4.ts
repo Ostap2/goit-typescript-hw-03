672d3a947383c268153ed952dde8979cdd3b3e4f
Ostap <Ostapkriven13@gmail.com>
2023-11-15 18:05:14 +0200

    і
    і

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,68 @@
-const key = new Key();
 
-const house = new MyHouse(key);
-const person = new Person(key);
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
 
-house.openDoor(person.getKey());
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
 
-house.comeIn(person);
+  abstract class House {
+    protected door: boolean = false; 
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log(`${person.getKey().getSignature()} entered the house.`);
+      } else {
+        console.log('The door is closed. Cannot enter.');
+      }
+    }
+  }
+  
 
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is open.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
 
-export {};
+  const key = new Key();
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  house.comeIn(person);
+  
